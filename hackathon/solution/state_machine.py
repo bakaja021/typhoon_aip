5b7a5f9640cbeb652a2677966d087790ecfa835e
ALojdl <andrej.lojdl@gmail.com>
2017-11-04 19:19:10 +0100

    FSM is set up and working, no real progress in price reduction made.

--- hackathon/solution/state_machine.py
+++ hackathon/solution/state_machine.py
@@ -44,69 +44,119 @@ class StateMachine(object):
         {'trigger': 'off',   'source': 'on_bad_empty',  'dest': 'off_bad_empty'},
 
         # off_good_full
-        {'trigger': 'half', 'source': 'off_good_full', 'dest': 'off_good_half'},
-        {'trigger': 'on', 'source': 'off_good_full', 'dest': 'on_good_full'},
-        {'trigger': 'bad', 'source': 'off_good_full', 'dest': 'off_bad_full'},
+        {'trigger': 'half',  'source': 'off_good_full', 'dest': 'off_good_half'},
+        {'trigger': 'on',    'source': 'off_good_full', 'dest': 'on_good_full'},
+        {'trigger': 'bad',   'source': 'off_good_full', 'dest': 'off_bad_full'},
 
         # off_good_half
-        {'trigger': 'full', 'source': 'off_good_half', 'dest': 'off_good_full'},
+        {'trigger': 'full',  'source': 'off_good_half', 'dest': 'off_good_full'},
         {'trigger': 'empty', 'source': 'off_good_half', 'dest': 'off_good_empty'},
-        {'trigger': 'on', 'source': 'off_good_half', 'dest': 'on_good_half'},
-        {'trigger': 'bad', 'source': 'off_good_half', 'dest': 'off_bad_half'},
+        {'trigger': 'on',    'source': 'off_good_half', 'dest': 'on_good_half'},
+        {'trigger': 'bad',   'source': 'off_good_half', 'dest': 'off_bad_half'},
 
         # off_good_empty
-        {'trigger': 'half', 'source': 'off_good_empty', 'dest': 'off_half_empty'},
-        {'trigger': 'on', 'source': 'off_good_empty', 'dest': 'on_good_empty'},
-        {'trigger': 'bad', 'source': 'off_good_empty', 'dest': 'off_bad_empty'},
+        {'trigger': 'half',  'source': 'off_good_empty', 'dest': 'off_half_empty'},
+        {'trigger': 'on',    'source': 'off_good_empty', 'dest': 'on_good_empty'},
+        {'trigger': 'bad',   'source': 'off_good_empty', 'dest': 'off_bad_empty'},
 
         # off_bad_full
-        {'trigger': 'half', 'source': 'off_bad_full', 'dest': 'off_bad_half'},
-        {'trigger': 'on', 'source': 'off_bad_full', 'dest': 'on_bad_full'},
-        {'trigger': 'good', 'source': 'off_bad_full', 'dest': 'off_good_full'},
+        {'trigger': 'half',  'source': 'off_bad_full', 'dest': 'off_bad_half'},
+        {'trigger': 'on',    'source': 'off_bad_full', 'dest': 'on_bad_full'},
+        {'trigger': 'good',  'source': 'off_bad_full', 'dest': 'off_good_full'},
 
         # off_bad_half
-        {'trigger': 'full', 'source': 'off_bad_half', 'dest': 'off_bad_full'},
+        {'trigger': 'full',  'source': 'off_bad_half', 'dest': 'off_bad_full'},
         {'trigger': 'empty', 'source': 'off_bad_half', 'dest': 'off_bad_empty'},
-        {'trigger': 'on', 'source': 'off_bad_half', 'dest': 'on_bad_half'},
-        {'trigger': 'good', 'source': 'off_bad_half', 'dest': 'off_good_half'},
+        {'trigger': 'on',    'source': 'off_bad_half', 'dest': 'on_bad_half'},
+        {'trigger': 'good',  'source': 'off_bad_half', 'dest': 'off_good_half'},
 
         # off_bad_empty
-        {'trigger': 'half', 'source': 'off_bad_empty', 'dest': 'off_bad_half'},
-        {'trigger': 'on', 'source': 'off_bad_empty', 'dest': 'on_bad_empty'},
-        {'trigger': 'good', 'source': 'off_bad_empty', 'dest': 'off_good_empty'},
+        {'trigger': 'half',  'source': 'off_bad_empty', 'dest': 'off_bad_half'},
+        {'trigger': 'on',    'source': 'off_bad_empty', 'dest': 'on_bad_empty'},
+        {'trigger': 'good',  'source': 'off_bad_empty', 'dest': 'off_good_empty'},
     ]
 
     def __init__(self):
-        self.machine = Machine(self, states=StateMachine.states, transitions=StateMachine.transitions, initial='mode1')
+        self.machine = Machine(self, states=StateMachine.states,
+                               transitions=StateMachine.transitions, initial='on_bad_half')
 
 
 class Handler(object):
     state_machine = StateMachine()
 
     def process(self, msg):
-        # pitam se da li se promenila mreza
+        state, price, battery = self.state_machine.state.split('_')
 
-        # pitam se da li se promenio odnos cena
-        # pitam se da li se popunjenost baterije promenila
+        # Ask if network changed status
+        curr_state = 'on' if msg.grid_status else 'off'
+        if curr_state != state:
+            if state == 'on':
+                self.state_machine.off()
+            else:
+                self.state_machine.on()
+
+        # Ask if prices changed
+        if msg.selling_price == msg.buying_price and price == 'bad' or \
+                                msg.selling_price < msg.buying_price and price == 'good':
+            if price == 'bad':
+                self.state_machine.good()
+            else:
+                self.state_machine.bad()
+
+        # Ask if fullness changed
+        if msg.bessSOC > 0.9:
+            curr_battery = 'full'
+        elif 0.25 <= msg.bessSOC <= 0.9:
+            curr_battery = 'half'
+        else:
+            curr_battery = 'empty'
+
+        if curr_battery != battery:
+            if battery != 'half':
+                self.state_machine.half()
+            else:
+                if curr_battery == 'full':
+                    self.state_machine.full()
+                else:
+                    self.state_machine.empty()
 
 
+        # For different states send different controls
+        if self.state_machine.state == 'on_good_full':
+            pow_ref = 6.0
+        elif self.state_machine.state == 'on_good_half':
+            pow_ref = 3.0
+        elif self.state_machine.state == 'on_good_empty':
+            pow_ref = 0.0
+        elif self.state_machine.state == 'on_bad_full':
+            pow_ref = 0.0
+        elif self.state_machine.state == 'on_bad_half':
+            pow_ref = 0.0
+        elif self.state_machine.state == 'on_bad_empty':
+            pow_ref = -3.0
+        elif self.state_machine.state == 'off_good_full':
+            pow_ref = 3.0
+        elif self.state_machine.state == 'off_good_half':
+            pow_ref = 0.0
+        elif self.state_machine.state == 'off_good_empty':
+            pow_ref = 0.0
+        elif self.state_machine.state == 'off_bad_full':
+            pow_ref = 0.0
+        elif self.state_machine.state == 'off_bad_half':
+            pow_ref = 0.0
+        else: #'off_bad_empty'
+            pow_ref = 0.0
 
-        if not msg.grid_status and self.state_machine.state == 'mode1':
-            self.state_machine.off()
-        elif msg.grid_status and self.state_machine.state == 'mode2':
-            self.state_machine.on()
-
-        if self.state_machine.state == 'mode1':
-            pv_mode = PVMode.OFF
-        elif self.state_machine.state == 'mode2':
-            pv_mode = PVMode.ON
-
+        pow_ref = 0.0
+        # Prepare response
         res = ResultsMessage(data_msg=msg,
                              load_one=True,
                              load_two=True,
                              load_three=True,
-                             power_reference=0.0,
-                             pv_mode=pv_mode)
+                             power_reference=pow_ref,
+                             pv_mode=PVMode.ON)
 
-        print('BamBam: {}'.format(res))
+        print('BamBam: {}'.format(msg))
+        print(self.state_machine.state)
+        print("=" * 50)
         return res
