91db4f4bece929108f37ccdb130aeb5f67f301a5
ALojdl <andrej.lojdl@gmail.com>
2017-11-05 10:50:18 +0100

    Created new state machine

--- hackathon/solution/state_machine.py
+++ hackathon/solution/state_machine.py
@@ -4,88 +4,111 @@ from hackathon.utils.utils import ResultsMessage, PVMode
 
 class StateMachine(object):
     states = [
-        'on_good_full' , 'on_good_half' , 'on_good_empty',
-        'on_bad_full'  , 'on_bad_half'  , 'on_bad_empty',
-        'off_good_full', 'off_good_half', 'off_good_empty',
-        'off_bad_full' , 'off_bad_half' , 'off_bad_empty'
+        'on_empty_none', 'on_empty_third', 'on_empty_secthr',
+        'on_half_none', 'on_half_third', 'on_half_secthr',
+        'on_full_none', 'on_full_third', 'on_full_secthr',
+        'off_empty_none', 'off_empty_third', 'off_empty_secthr',
+        'off_half_none', 'off_half_third', 'off_half_secthr',
+        'off_full_none', 'off_full_third', 'off_full_secthr'
     ]
 
     transitions = [
-        # on_good_full
-        {'trigger': 'off',   'source': 'on_good_full',  'dest': 'off_good_full'},
-        {'trigger': 'bad',   'source': 'on_good_full',  'dest': 'on_bad_full'},
-        {'trigger': 'half',  'source': 'on_good_full',  'dest': 'on_good_half'},
-
-        # on_good_half
-        {'trigger': 'full',  'source': 'on_good_half',  'dest': 'on_good_full'},
-        {'trigger': 'empty', 'source': 'on_good_half',  'dest': 'on_good_empty'},
-        {'trigger': 'bad',   'source': 'on_good_half',  'dest': 'on_bad_half'},
-        {'trigger': 'off',   'source': 'on_good_half',  'dest': 'off_good_half'},
-
-        # on_good_empty
-        {'trigger': 'half',  'source': 'on_good_empty', 'dest': 'on_good_half'},
-        {'trigger': 'bad',   'source': 'on_good_empty', 'dest': 'on_bad_empty'},
-        {'trigger': 'off',   'source': 'on_good_empty', 'dest': 'off_good_empty'},
-
-        # on_bad_full
-        {'trigger': 'good',  'source': 'on_bad_full',   'dest': 'on_good_full'},
-        {'trigger': 'half',  'source': 'on_bad_full',   'dest': 'on_bad_half'},
-        {'trigger': 'off',   'source': 'on_bad_full',   'dest': 'off_bad_full'},
-
-        # on_bad_half
-        {'trigger': 'full',  'source': 'on_bad_half',   'dest': 'on_bad_full'},
-        {'trigger': 'good',  'source': 'on_bad_half',   'dest': 'on_good_half'},
-        {'trigger': 'empty', 'source': 'on_bad_half',   'dest': 'on_bad_empty'},
-        {'trigger': 'off',   'source': 'on_bad_half',   'dest': 'off_bad_half'},
-
-        # on_bad_empty
-        {'trigger': 'good',  'source': 'on_bad_empty',  'dest': 'on_good_empty'},
-        {'trigger': 'half',  'source': 'on_bad_empty',  'dest': 'on_bad_half'},
-        {'trigger': 'off',   'source': 'on_bad_empty',  'dest': 'off_bad_empty'},
-
-        # off_good_full
-        {'trigger': 'half',  'source': 'off_good_full', 'dest': 'off_good_half'},
-        {'trigger': 'on',    'source': 'off_good_full', 'dest': 'on_good_full'},
-        {'trigger': 'bad',   'source': 'off_good_full', 'dest': 'off_bad_full'},
-
-        # off_good_half
-        {'trigger': 'full',  'source': 'off_good_half', 'dest': 'off_good_full'},
-        {'trigger': 'empty', 'source': 'off_good_half', 'dest': 'off_good_empty'},
-        {'trigger': 'on',    'source': 'off_good_half', 'dest': 'on_good_half'},
-        {'trigger': 'bad',   'source': 'off_good_half', 'dest': 'off_bad_half'},
-
-        # off_good_empty
-        {'trigger': 'half',  'source': 'off_good_empty', 'dest': 'off_half_empty'},
-        {'trigger': 'on',    'source': 'off_good_empty', 'dest': 'on_good_empty'},
-        {'trigger': 'bad',   'source': 'off_good_empty', 'dest': 'off_bad_empty'},
-
-        # off_bad_full
-        {'trigger': 'half',  'source': 'off_bad_full', 'dest': 'off_bad_half'},
-        {'trigger': 'on',    'source': 'off_bad_full', 'dest': 'on_bad_full'},
-        {'trigger': 'good',  'source': 'off_bad_full', 'dest': 'off_good_full'},
-
-        # off_bad_half
-        {'trigger': 'full',  'source': 'off_bad_half', 'dest': 'off_bad_full'},
-        {'trigger': 'empty', 'source': 'off_bad_half', 'dest': 'off_bad_empty'},
-        {'trigger': 'on',    'source': 'off_bad_half', 'dest': 'on_bad_half'},
-        {'trigger': 'good',  'source': 'off_bad_half', 'dest': 'off_good_half'},
-
-        # off_bad_empty
-        {'trigger': 'half',  'source': 'off_bad_empty', 'dest': 'off_bad_half'},
-        {'trigger': 'on',    'source': 'off_bad_empty', 'dest': 'on_bad_empty'},
-        {'trigger': 'good',  'source': 'off_bad_empty', 'dest': 'off_good_empty'},
+        # on_empty_none
+        {'trigger': 'off', 'source': 'on_empty_none', 'dest': 'off_empty_none'},
+        {'trigger': 'half', 'source': 'on_empty_none', 'dest': 'on_half_none'},
+        {'trigger': 'third', 'source': 'on_empty_none', 'dest': 'on_empty_third'},
+        # on_empty_third
+        {'trigger': 'off', 'source': 'on_empty_third', 'dest': 'off_empty_third'},
+        {'trigger': 'half', 'source': 'on_empty_third', 'dest': 'on_half_third'},
+        {'trigger': 'none', 'source': 'on_empty_third', 'dest': 'on_empty_none'},
+        {'trigger': 'secthr', 'source': 'on_empty_third', 'dest': 'on_empty_secthr'},
+        # on_empty_secthr
+        {'trigger': 'off', 'source': 'on_empty_secthr', 'dest': 'off_empty_secthr'},
+        {'trigger': 'half', 'source': 'on_empty_secthr', 'dest': 'on_half_secthr'},
+        {'trigger': 'third', 'source': 'on_empty_secthr', 'dest': 'on_empty_third'},
+        # on_half_none
+        {'trigger': 'off', 'source': 'on_half_none', 'dest': 'off_half_none'},
+        {'trigger': 'full', 'source': 'on_half_none', 'dest': 'on_full_none'},
+        {'trigger': 'empty', 'source': 'on_half_none', 'dest': 'on_empty_none'},
+        {'trigger': 'third', 'source': 'on_half_none', 'dest': 'on_half_third'},
+        # on_half_third
+        {'trigger': 'off', 'source': 'on_half_third', 'dest': 'off_half_third'},
+        {'trigger': 'full', 'source': 'on_half_third', 'dest': 'on_full_third'},
+        {'trigger': 'empty', 'source': 'on_half_third', 'dest': 'on_empty_third'},
+        {'trigger': 'none', 'source': 'on_half_third', 'dest': 'on_half_none'},
+        {'trigger': 'secthr', 'source': 'on_half_third', 'dest': 'on_half_secthr'},
+        # on_half_secthr
+        {'trigger': 'off', 'source': 'on_half_secthr', 'dest': 'off_half_secthr'},
+        {'trigger': 'full', 'source': 'on_half_secthr', 'dest': 'on_full_secthr'},
+        {'trigger': 'empty', 'source': 'on_half_secthr', 'dest': 'on_empty_secthr'},
+        {'trigger': 'third', 'source': 'on_half_secthr', 'dest': 'on_half_third'},
+        # on_full_none
+        {'trigger': 'off', 'source': 'on_full_none', 'dest': 'off_full_none'},
+        {'trigger': 'half', 'source': 'on_full_none', 'dest': 'on_half_none'},
+        {'trigger': 'third', 'source': 'on_full_none', 'dest': 'on_full_third'},
+        # on_full_third
+        {'trigger': 'off', 'source': 'on_full_third', 'dest': 'off_full_third'},
+        {'trigger': 'half', 'source': 'on_full_third', 'dest': 'on_half_third'},
+        {'trigger': 'secthr', 'source': 'on_full_third', 'dest': 'on_full_secthr'},
+        {'trigger': 'none', 'source': 'on_full_third', 'dest': 'on_full_none'},
+        # on_full_secthr
+        {'trigger': 'off', 'source': 'on_full_secthr', 'dest': 'off_full_secthr'},
+        {'trigger': 'half', 'source': 'on_full_secthr', 'dest': 'on_half_secthr'},
+        {'trigger': 'third', 'source': 'on_full_secthr', 'dest': 'on_full_third'},
+        # off_empty_none
+        {'trigger': 'on', 'source': 'off_empty_none', 'dest': 'on_empty_none'},
+        {'trigger': 'half', 'source': 'off_empty_none', 'dest': 'off_half_none'},
+        {'trigger': 'third', 'source': 'off_empty_none', 'dest': 'off_empty_third'},
+        # off_empty_third
+        {'trigger': 'on', 'source': 'off_empty_third', 'dest': 'on_empty_third'},
+        {'trigger': 'half', 'source': 'off_empty_third', 'dest': 'off_half_third'},
+        {'trigger': 'secthr', 'source': 'off_empty_third', 'dest': 'off_empty_secthr'},
+        {'trigger': 'none', 'source': 'off_empty_third', 'dest': 'off_empty_none'},
+        # off_empty_secthr
+        {'trigger': 'on', 'source': 'off_empty_secthr', 'dest': 'on_empty_secthr'},
+        {'trigger': 'half', 'source': 'off_empty_secthr', 'dest': 'off_half_secthr'},
+        {'trigger': 'third', 'source': 'off_empty_secthr', 'dest': 'off_empty_third'},
+        # off_half_none
+        {'trigger': 'on', 'source': 'off_half_none', 'dest': 'on_half_none'},
+        {'trigger': 'full', 'source': 'off_half_none', 'dest': 'off_full_none'},
+        {'trigger': 'empty', 'source': 'off_half_none', 'dest': 'off_empty_none'},
+        {'trigger': 'third', 'source': 'off_half_none', 'dest': 'off_half_third'},
+        # off_half_third
+        {'trigger': 'on', 'source': 'off_half_third', 'dest': 'on_half_third'},
+        {'trigger': 'full', 'source': 'off_half_third', 'dest': 'off_full_third'},
+        {'trigger': 'empty', 'source': 'off_half_third', 'dest': 'off_empty_third'},
+        {'trigger': 'secthr', 'source': 'off_half_third', 'dest': 'off_half_secthr'},
+        {'trigger': 'none', 'source': 'off_half_third', 'dest': 'off_half_none'},
+        # off_half_secthr
+        {'trigger': 'on', 'source': 'off_half_secthr', 'dest': 'on_half_secthr'},
+        {'trigger': 'full', 'source': 'off_half_secthr', 'dest': 'off_full_secthr'},
+        {'trigger': 'empty', 'source': 'off_half_secthr', 'dest': 'off_empty_secthr'},
+        {'trigger': 'third', 'source': 'off_half_secthr', 'dest': 'off_half_third'},
+        # off_full_none
+        {'trigger': 'on', 'source': 'off_full_none', 'dest': 'off_full_none'},
+        {'trigger': 'half', 'source': 'off_full_none', 'dest': 'off_half_none'},
+        {'trigger': 'third', 'source': 'off_full_none', 'dest': 'off_full_third'},
+        # off_full_third
+        {'trigger': 'on', 'source': 'off_full_third', 'dest': 'on_full_third'},
+        {'trigger': 'half', 'source': 'off_full_third', 'dest': 'off_half_third'},
+        {'trigger': 'secthr', 'source': 'off_full_third', 'dest': 'off_full_secthr'},
+        {'trigger': 'none', 'source': 'off_full_third', 'dest': 'off_full_none'},
+        # off_full_secthr
+        {'trigger': 'on', 'source': 'off_full_secthr', 'dest': 'on_full_secthr'},
+        {'trigger': 'half', 'source': 'off_full_secthr', 'dest': 'off_half_secthr'},
+        {'trigger': 'third', 'source': 'off_full_secthr', 'dest': 'off_full_third'}
     ]
 
     def __init__(self):
         self.machine = Machine(self, states=StateMachine.states,
-                               transitions=StateMachine.transitions, initial='on_bad_half')
+                               transitions=StateMachine.transitions, initial='on_half_none')
 
 
 class Handler(object):
     state_machine = StateMachine()
 
     def process(self, msg):
-        state, price, battery = self.state_machine.state.split('_')
+        state, battery, disabled = self.state_machine.state.split('_')
 
         # Ask if network changed status
         curr_state = 'on' if msg.grid_status else 'off'
@@ -95,18 +118,10 @@ class Handler(object):
             else:
                 self.state_machine.on()
 
-        # Ask if prices changed
-        if msg.selling_price == msg.buying_price and price == 'bad' or \
-                                msg.selling_price < msg.buying_price and price == 'good':
-            if price == 'bad':
-                self.state_machine.good()
-            else:
-                self.state_machine.bad()
-
         # Ask if fullness changed
-        if msg.bessSOC > 0.9:
+        if msg.bessSOC > 0.8:
             curr_battery = 'full'
-        elif 0.25 <= msg.bessSOC <= 0.9:
+        elif 0.2 <= msg.bessSOC <= 0.8:
             curr_battery = 'half'
         else:
             curr_battery = 'empty'
@@ -120,43 +135,17 @@ class Handler(object):
                 else:
                     self.state_machine.empty()
 
+        # Ask if some device should be powered on or of
 
-        # For different states send different controls
-        if self.state_machine.state == 'on_good_full':
-            pow_ref = 6.0
-        elif self.state_machine.state == 'on_good_half':
-            pow_ref = 3.0
-        elif self.state_machine.state == 'on_good_empty':
-            pow_ref = 0.0
-        elif self.state_machine.state == 'on_bad_full':
-            pow_ref = 0.0
-        elif self.state_machine.state == 'on_bad_half':
-            pow_ref = 0.0
-        elif self.state_machine.state == 'on_bad_empty':
-            pow_ref = -3.0
-        elif self.state_machine.state == 'off_good_full':
-            pow_ref = 3.0
-        elif self.state_machine.state == 'off_good_half':
-            pow_ref = 0.0
-        elif self.state_machine.state == 'off_good_empty':
-            pow_ref = 0.0
-        elif self.state_machine.state == 'off_bad_full':
-            pow_ref = 0.0
-        elif self.state_machine.state == 'off_bad_half':
-            pow_ref = 0.0
-        else: #'off_bad_empty'
-            pow_ref = 0.0
-
-        pow_ref = 0.0
         # Prepare response
         res = ResultsMessage(data_msg=msg,
                              load_one=True,
                              load_two=True,
                              load_three=True,
-                             power_reference=pow_ref,
+                             power_reference=0.0,
                              pv_mode=PVMode.ON)
 
-        print('BamBam: {}'.format(msg))
+        print('BamBam: {}'.format(res))
         print(self.state_machine.state)
         print("=" * 50)
         return res
